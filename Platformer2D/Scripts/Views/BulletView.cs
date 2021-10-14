c8b4b52e9a38792be06dfb5e19d414fe609afba3
ArtyomBolshedonov <blizart@yandex.ru>
2021-10-14 21:39:56 +0300

    Homework3
    1. Переделал персонажа и уровень так, чтобы они использовали физику Box2D.
    Создал более длинный уровень с монетками, death-зонами.
    2. Сделал стрельбу из пушек опасной для персонажа, при попадании в него трёх пуль также переносите его в начало уровня.

---
+++ Platformer2D/Scripts/Views/BulletView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+
+namespace Platformer2D
+{
+    internal sealed class BulletView : ObjectView
+    {
+        [SerializeField]
+        private TrailRenderer _trail;
+
+        public void SetVisible(bool visible)
+        {
+            if (_trail) _trail.enabled = visible;
+            if (_trail) _trail.Clear();
+            SpriteRenderer.enabled = visible;
+            Rigidbody2D.simulated = visible;
+            Collider2D.enabled = visible;
+        }
+
+        protected override SpriteAnimationConfig GetSpriteAnimationConfig()
+        {
+            return Resources.Load<SpriteAnimationConfig>("BulletAnimationConfig");
+        }
+    }
+}
